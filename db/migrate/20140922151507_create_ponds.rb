d91443fa1bac2b2a2a052ae361fd10b62ae5b317
Brittany Morris <brittmmendez@gmail.com>
2018-02-27 02:50:22 +0000

    automatically backed up by learn

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -3,9 +3,6 @@ class CreatePonds < ActiveRecord::Migration
       create_table :ponds do |t|
         t.string :name
         t.string :water_type
-
-
-        t.timestamps null: false
       end
     end
 end
