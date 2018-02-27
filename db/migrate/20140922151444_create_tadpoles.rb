97d82bfc08f06cf9671c18a9edef37360de1f6b0
Brittany Morris <brittmmendez@gmail.com>
2018-02-27 02:50:01 +0000

    automatically backed up by learn

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -4,8 +4,6 @@ class CreateTadpoles < ActiveRecord::Migration
         t.string :name
         t.string :color
         t.integer :frog_id
-
-        t.timestamps null: false
       end
     end
 end
