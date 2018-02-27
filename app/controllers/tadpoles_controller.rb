e3f8523175493dee18547ecb5c40e60ea3ce1fb6
Brittany Morris <brittmmendez@gmail.com>
2018-02-27 03:17:54 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -4,7 +4,7 @@ class TadpolesController < ApplicationController
   def metamorphose
     set_tadpole
     #makes a new frog with the tadpole's name, color, and pond
-    @frog = Frog.new(name: "#{@tadpole.name}", color: "#{@tadpole.color}",  pond: @tadpole.pond_id)
+    @frog = Frog.create(name: "#{@tadpole.name}", color: "#{@tadpole.color}",  pond: @tadpole.pond_id)
     #deletes the tadpole from the database
     @tadpole.delete
     #redirects to the newly made frog's show page
